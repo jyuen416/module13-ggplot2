cfb3ec77c63197a625d20206f6bb914edd8e51cd
Jessica Yuen <jyuen416@uw.edu>
2017-02-07 15:18:36 -0800

    classwork

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,19 +1,20 @@
 # Exercise 2: More ggplot2 Grammar
 
 # Install and load `ggplot2`
-# install.packages("ggplot2") # if needed
+library("ggplot2")
 
 
 # For this exercise you will again be working with the `diamonds` data set.
 # Use `?diamonds` to review details about this data set
-
+?diamonds
 
 
 ## Statistical Transformations
 
 # Draw a bar chart of the diamonds data, organized by cut
 # The height of each bar is based on the "count" (number) of diamonds with that cut
-
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut), stat = "count")
 
 # Use the `stat_count` to apply the statistical transformation "count" to the diamonds
 # by cut. You do not need a separate geometry layer!
